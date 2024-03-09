f3a74151cd7ee2334d407603a697bb5fd6477e1a
Andnm <dangnguyenminhan123@gmail.com>
2024-03-09 15:46:18 +0700

    push chat app

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,112 @@
+import 'package:chat_application/services/auth/auth_service.dart';
+import 'package:chat_application/components/my_button.dart';
+import 'package:chat_application/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+
+  final void Function()? onTap;
+
+  LoginPage({
+    super.key,
+    required this.onTap,
+  });
+
+  void login(BuildContext context) async {
+    final authService = AuthService();
+
+    try {
+      await authService.signInWithEmailPassword(
+        _emailController.text,
+        _pwController.text,
+      );
+    } catch (e) {
+      showDialog(
+        // ignore: use_build_context_synchronously
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(e.toString()),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            //welcome
+            Text(
+              "Welcome back, you've been missed!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            //email field
+            MyTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
+
+            const SizedBox(height: 10),
+            //pwd field
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _pwController,
+            ),
+
+            //login button
+            const SizedBox(height: 25),
+
+            MyButton(
+              text: 'Login',
+              onTap: () => login(context),
+            ),
+
+            const SizedBox(height: 25),
+
+            //register
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Not a member?",
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Text(
+                    "Register now",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
